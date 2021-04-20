17814bec546dc6295e8953daaa7f2e6a3fedff13
Valeriya Reva <valeriyareva@Valeriyas-MacBook-Pro.local>
2021-04-20 17:43:51 +0300

    added calc function

---
+++ bit_calc/checksum.h
@@ -0,0 +1,6 @@
+#ifndef CHECKSUM_H
+#define CHECKSUM_H
+
+int count_bit(char b);
+
+#endif
---
+++ bit_calc/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <checksum.h>
+using namespace std;
+
+
+int main()
+{
+    char counted_chat = 't';
+    cout << "Counted bit length " << count_bit(counted_chat);
+    return 0;
+}
