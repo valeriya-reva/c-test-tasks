0293868cf2c92992ff9eb2aae2e7c8605e23ee5e
Valeriya Reva <valeriyareva@Valeriyas-MacBook-Pro.local>
2021-04-21 20:10:50 +0300

    fixed sintax issues

--- bit_calc/checksum.cpp
+++ bit_calc/checksum.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
-using namespace std;
-
 int count_bit(char b) {
     int count = 0;
     for (int i = 7; i >= 0; --i) {
-        if((b >> i) & 1) count += 1;
+        if((b >> i) & 1) {
+            count += 1;
+        }
     };
 
     return count;
